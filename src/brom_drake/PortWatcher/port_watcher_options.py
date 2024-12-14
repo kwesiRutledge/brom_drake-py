49ff91a75de3c1868dfeff7565b79c83032d0b9d
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2024-12-14 19:09:13 -0400

    Added some initial comments for the set of files associated with ProximityPosePlanDispenser object

--- src/brom_drake/PortWatcher/port_watcher_options.py
+++ src/brom_drake/PortWatcher/port_watcher_options.py
@@ -13,6 +13,11 @@ from brom_drake.directories import DEFAULT_PLOT_DIR, DEFAULT_RAW_DATA_DIR, DEFAU
 from .port_figure_arrangement import PortFigureArrangement
 
 class FigureNamingConvention(IntEnum):
+    """
+    Description
+    -----------
+    This enum is used to define the naming convention for the figures.
+    """
     kFlat = 0 # e.g. "plant_generalized_output_dim_0.png"
     kHierarchical = 1 # e.g. "system_plant/port_generalized_output/dim_0.png"
 
