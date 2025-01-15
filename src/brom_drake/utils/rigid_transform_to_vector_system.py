7434b1dd3302d5241a7a0331b44b4fce92e48a55
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-01-14 20:58:47 -0500

    Improved comments on some of our objects

--- src/brom_drake/utils/rigid_transform_to_vector_system.py
+++ src/brom_drake/utils/rigid_transform_to_vector_system.py
@@ -7,6 +7,22 @@ from pydrake.all import (
 )
 
 class RigidTransformToVectorSystem(LeafSystem):
+    """
+    Description
+    -----------
+    This system will take in a RigidTransform and output a vector
+    of size 7, where the first 3 elements are the translation
+    and the last 4 elements are the quaternion representation
+    of the rotation.
+
+    Diagram
+    -------
+                            |-------------------|
+                            | RigidTransform    |
+    rigid_transform ---->   | To                | vector_xyz_quat(wxyz) (BasicVector[7])
+    (RigidTransform)        | VectorSystem      |
+                            |-------------------|
+    """
     def __init__(self):
         LeafSystem.__init__(self)
         self.rigid_transform_input = self.DeclareAbstractInputPort(
