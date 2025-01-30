3cd9cdb8afeda088a26170ff34e694d3de3e3339
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-01-30 12:49:20 -0500

    Added test for casting and build ChemLab2

--- tests/productions/motion_planning/offline/dynamic/chem_lab2_test.py
+++ tests/productions/motion_planning/offline/dynamic/chem_lab2_test.py
@@ -1,6 +1,12 @@
+from pydrake.all import Simulator
 import unittest
 
 # Internal Imports
+from brom_drake.motion_planning.algorithms.rrt.bidirectional_connect import (
+    BidirectionalRRTConnectPlanner,
+    BidirectionalRRTConnectPlannerConfig,
+    BiRRTConnectSamplingProbabilities,
+)
 from brom_drake.productions.motion_planning.offline.dynamic.chem_lab2 import ChemLab2
 
 class ChemLab2Test(unittest.TestCase):
@@ -15,6 +21,55 @@ class ChemLab2Test(unittest.TestCase):
             meshcat_port_number=None,
         )
 
+        self.assertTrue(True)
+
+    def test_easy_cast_and_build1(self):
+        """
+        Description
+        -----------
+        This test verifies that we can build the ChemLab2 without any errors.
+        """
+        # Setup
+        
+        # Create the production
+        production = ChemLab2(
+            meshcat_port_number=None, # Use None for CI
+        )
+
+        # Create a planner object which will be used to plan the motion
+        config = BidirectionalRRTConnectPlannerConfig(
+            steering_step_size=0.2,
+        )
+        planner2 = BidirectionalRRTConnectPlanner(
+            production.arm,
+            production.plant,
+            production.scene_graph,
+            config=config,
+        )
+
+        # To build the production, we only need to provide a planning function
+        # (can come from anywhere, not just a BaseRRTPlanner object)
+        diagram, diagram_context = production.easy_cast_and_build(
+            planner2.plan,
+            with_watcher=True,
+        )
+
+        print("Simulating...")
+
+        # Simulate the diagram
+        simulator = Simulator(diagram, diagram_context)
+        simulator.set_target_realtime_rate(2.0)
+
+        # Run simulation
+        simulator.Initialize()
+        simulator.AdvanceTo(0.05)
+        planned_trajectory = production.plan_dispenser.planned_trajectory
+        print(f"Expected end time of trajectory: {planned_trajectory.end_time()}")
+        simulator.AdvanceTo(0.1)
+        # return
+        simulator.AdvanceTo(1.0)
+
+        # Return true if we get here
         self.assertTrue(True)
 
 if __name__ == "__main__":
