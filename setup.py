ea186b0919ff778a4ca155515202617de6005d8e
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-03-12 14:05:49 -0400

    Adding pycollada back

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         install_requires=[
             'drake', 'meshcat',
             'loguru', 'matplotlib', 'numpy',
-            'trimesh',
+            'pycollada', 'trimesh',
         ],
         keywords=["drake", "robotics", "testing", "logging"],
         classifiers=[
