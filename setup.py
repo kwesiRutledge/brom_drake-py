fe7e7ab9111588b5f1544537c9c14cbf03ad76b3
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-03-12 13:57:04 -0400

    Remove pycollada dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         install_requires=[
             'drake', 'meshcat',
             'loguru', 'matplotlib', 'numpy',
-            'pycollada', 'trimesh',
+            'trimesh',
         ],
         keywords=["drake", "robotics", "testing", "logging"],
         classifiers=[
