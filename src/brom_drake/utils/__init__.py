5b5f3b97d72303aa13d22dff3e395aad9315cec0
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-03-22 13:03:28 -0400

    Exposing the collision checking functions to broader utils module

--- src/brom_drake/utils/__init__.py
+++ src/brom_drake/utils/__init__.py
@@ -6,10 +6,12 @@ from .leaf_systems.bool_to_vec_system import BoolToVectorSystem
 from .leaf_systems.end_effector_wrench_calculator import EndEffectorWrenchCalculator
 from .leaf_systems.rigid_transform_to_vector_system import RigidTransformToVectorSystem
 from .constants import Performer, MotionPlan
+from .collision_checking import using_point_pair_penetration
 from .ground import AddGround, GroundShape
 
 __all__ = [
     "BoolToVectorSystem",
+    "using_point_pair_penetration",
     "EndEffectorWrenchCalculator",
     "find_all_systems_with_input_port",
     "find_all_systems_with_output_port",
