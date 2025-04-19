75a5c469938fd66e319a7bea81a88382face27f8
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-04-19 11:15:04 -0400

    Added a new system to the brom_drake.leaf_systems library

--- src/brom_drake/leaf_systems/__init__.py
+++ src/brom_drake/leaf_systems/__init__.py
@@ -1,1 +1,7 @@
 from brom_drake.utils.leaf_systems.flexible_port_switch import FlexiblePortSwitch
+from brom_drake.utils.leaf_systems.gripper_controller import GripperController
+
+__all__ = [
+    "FlexiblePortSwitch",
+    "GripperController",
+]
