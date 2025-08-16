da31ba26d6b1966e7847f147293df2ec992512a4
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2025-08-16 12:39:02 -0400

    Sharing more of the shape definitions in the shapes package

---
+++ src/brom_drake/file_manipulation/urdf/shapes/__init__.py
@@ -0,0 +1,11 @@
+from .box import BoxDefinition
+from .cylinder import CylinderDefinition
+from .shape_definition import ShapeDefinition, ShapeEnum
+from .sphere import SphereDefinition
+
+__all__ = [
+    "BoxDefinition",
+    "CylinderDefinition",
+    "ShapeDefinition", "ShapeEnum",
+    "SphereDefinition",
+]
