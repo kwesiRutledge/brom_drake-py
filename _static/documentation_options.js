dfbd706b58ecef782281c8097e43d52c268bc831
kwesiRutledge <kwesiRutledge@users.noreply.github.com>
2025-12-24 04:56:54 +0000

    deploy: 236c25900e9181da6fcd6c9c3238f37b490b0a49

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v0.0.0+sphinxFallback',
+    VERSION: 'v0.0.0+githubActionDefault',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
