3f58d2ebe96efc0a4e88a47064902b90cb5acb51
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2026-01-05 21:06:20 -0500

    Updated comments to fix issues/errors in source code

--- src/brom_drake/directories.py
+++ src/brom_drake/directories.py
@@ -1,8 +1,8 @@
 
 
 DEFAULT_BROM_DIR = "./brom" #: Default directories for BROM Drake data storage
-DEFAULT_WATCHER_DIR = DEFAULT_BROM_DIR + "/watcher" #: Default subdirectory for PortWatcher data
-DEFAULT_PLOT_DIR = DEFAULT_WATCHER_DIR + "/plots"
-DEFAULT_RAW_DATA_DIR = DEFAULT_WATCHER_DIR + "/raw_data"
-DEFAULT_BROM_MODELS_DIR = DEFAULT_BROM_DIR + "/models"
-DEFAULT_NETWORKX_FSM_DIR = DEFAULT_BROM_DIR + "/networkx_fsm"
+DEFAULT_WATCHER_DIR = DEFAULT_BROM_DIR + "/watcher" #: Default subdirectory for :py:class:`PortWatcher<brom_drake.PortWatcher.port_watcher.PortWatcher>` data
+DEFAULT_PLOT_DIR = DEFAULT_WATCHER_DIR + "/plots" #: Default subdirectory for :py:class:`PortWatcher<brom_drake.PortWatcher.port_watcher.PortWatcher>` plots
+DEFAULT_RAW_DATA_DIR = DEFAULT_WATCHER_DIR + "/raw_data" #: Default subdirectory for :py:class:`PortWatcher<brom_drake.PortWatcher.port_watcher.PortWatcher>` raw data
+DEFAULT_BROM_MODELS_DIR = DEFAULT_BROM_DIR + "/models" #: Default subdirectory for BROM Drake models
+DEFAULT_NETWORKX_FSM_DIR = DEFAULT_BROM_DIR + "/networkx_fsm" #: Default subdirectory for NetworkX FSM data
