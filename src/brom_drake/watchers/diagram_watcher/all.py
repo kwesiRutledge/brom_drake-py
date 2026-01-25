5427ae70bf53922ef1b1de338dd87d149c054bfa
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2026-01-25 13:04:26 -0500

    Updated the __all__ variable in a few more places

--- src/brom_drake/watchers/diagram_watcher/all.py
+++ src/brom_drake/watchers/diagram_watcher/all.py
@@ -2,3 +2,11 @@ from .constants import INELIGIBLE_SYSTEM_TYPES
 from .diagram_watcher_options import DiagramWatcherOptions, SuppressDiagramWatcherRules
 from .diagram_watcher import DiagramWatcher
 from .errors import UnrecognizedTargetError
+
+__all__ = [
+    'DiagramWatcher',
+    'DiagramWatcherOptions',
+    'INELIGIBLE_SYSTEM_TYPES',
+    'SuppressDiagramWatcherRules',
+    'UnrecognizedTargetError',
+]
