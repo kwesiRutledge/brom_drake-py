c11e42fded443d9e9bc20662a9c891cfe5cc251c
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2026-05-16 19:48:14 -0400

    fixing metadata recording

--- src/brom_drake/productions/types/motion_planning/offline/kinematic.py
+++ src/brom_drake/productions/types/motion_planning/offline/kinematic.py
@@ -534,9 +534,7 @@ class KinematicMotionPlanningProduction(BaseProduction):
         if "leaf_systems" not in self._metadata:
             self._metadata["leaf_systems"] = {}
 
-        self._metadata["leaf_systems"]["plant"] = create_summary_for_LeafSystem(
-            self.plant
-        )
+        self._metadata["leaf_systems"]["plant"] = create_summary_for_LeafSystem(self.plant)
 
         return super()._record_metadata()
 
--- src/brom_drake/utils/metadata_recording.py
+++ src/brom_drake/utils/metadata_recording.py
@@ -8,7 +8,13 @@ def create_summary_for_LeafSystem(leaf_system: LeafSystem) -> dict:
     """
     summary = {
         "name": leaf_system.GetSystemName(),
-        "input_ports": [port.GetName() for port in leaf_system.get_input_ports()],
-        "output_ports": [port.GetName() for port in leaf_system.get_output_ports()],
+        "input_ports": [
+            leaf_system.get_input_port(input_port_index).GetName() 
+            for input_port_index in range(leaf_system.num_input_ports())
+        ],
+        "output_ports": [
+            leaf_system.get_output_port(output_port_index).GetName() 
+            for output_port_index in range(leaf_system.num_output_ports())
+        ],
     }
     return summary
